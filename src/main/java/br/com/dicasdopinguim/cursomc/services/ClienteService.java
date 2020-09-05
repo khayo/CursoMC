b07c7511ec3e7708f5c8564edaec3d3b36af181f
khayo <khayovannucci@gmail.com>
2020-09-05 18:09:55 -0300

    Endpoint /clientes/{id} disponível

---
+++ src/main/java/br/com/dicasdopinguim/cursomc/services/ClienteService.java
@@ -0,0 +1,22 @@
+package br.com.dicasdopinguim.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.dicasdopinguim.cursomc.domain.Cliente;
+import br.com.dicasdopinguim.cursomc.repositories.ClienteRepository;
+import br.com.dicasdopinguim.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
